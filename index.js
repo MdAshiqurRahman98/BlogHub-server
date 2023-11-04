f4dbcfba4ad527bb983431285d8c904bc887500c
MdAshiqurRahman98 <MDASHIQUROFFICIAL@gmail.com>
2023-11-05 00:55:35 +0600

    verify jwt added

--- index.js
+++ index.js
@@ -26,6 +26,24 @@ const logger = async (req, res, next) => {
     next();
 }
 
+const verifyToken = async (req, res, next) => {
+    const token = req?.cookies?.token;
+    // console.log('Token in the middleware: ', token);
+
+    if (!token) {
+        return res.status(401).send({ message: 'unauthorized access' });
+    }
+
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+            return res.status(401).send({ message: 'unauthorized access' });
+        }
+
+        req.user = decoded;
+        next();
+    })
+}
+
 async function run() {
     try {
         // Auth related API
